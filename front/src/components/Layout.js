60a22d63efafc81f678ca85d13a02c80bf17bb84
Jean Lamarre <jeanlamarre16@yahoo.fr>
2026-04-24 17:21:11 +0200

    refactor: mise en place des règles de validation (mot passe - code 2fa) + affichage dynamique nom utilisateur lors de la connexion

--- front/src/components/Layout.js
+++ front/src/components/Layout.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, username, step }) => {
+  const getSubTitle = () => {
+    if (step === 'welcome' || step === 'dashboard') {
+      return `Bonjour, ${username}`;
+    }
+    return "Authentification";
+  };
+
   return (
     <div className="page-wrapper">
       <main className="container" role="main">
         <header>
           <h1>COFRAP Cloud</h1>
-          <p>Authentification</p>
+          <p>{getSubTitle()}</p>
         </header>
         {children}
       </main>
